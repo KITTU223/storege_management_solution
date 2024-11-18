65109a27ac74aff565a70ba0b3726cdf78e4eda4
Krutarth Chauhan <63405029+KITTU223@users.noreply.github.com>
2024-11-19 00:13:04 +0530

    first commit

---
+++ app/(root)/[type]/page.tsx
@@ -0,0 +1,44 @@
+import Card from '@/components/Card';
+import Short from '@/components/Short';
+import { getFiles } from '@/lib/actions/file.actions';
+import { getFileTypesParams } from '@/lib/utils';
+import { Models } from 'node-appwrite';
+import React from 'react'
+
+const page = async ({searchParams,params}:SearchParamProps) => {
+    const type = (await params)?.type as string || '';
+    const searchText = ((await searchParams)?.query as string) || '';
+    const sort = ((await searchParams)?.sort as string) || '';
+
+    const types = getFileTypesParams(type) as FileType[];
+
+    const files =  await getFiles({ types, searchText,sort });
+
+  return (
+    <div className='page-container'>
+        <section className='w-full'>
+            <h1 className='h1 capitalize'>{type}</h1>
+            <div className='total-size-section'>
+                <p className='body-1'>
+                    Total: <span className='h5'>0 MB</span>
+                </p>
+
+                <div className='short-container'>
+                    <p className='body-1 hidden sm:block text-light-200'>Short By:</p>
+                    <Short/>
+                </div>
+            </div>
+        </section>
+        {/*Render the files*/}
+        {files.total > 0 ? (
+            <section className='file-list'>
+                {files.documents.map((file: Models.Document) => (
+                    <Card key={file.$id} file={file} />
+                ))}
+            </section>
+        ): <p className='empy-list'>No files uploaded</p>}
+    </div>
+  )
+}
+
+export default page
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,128 @@
+"use server";
+
+import { ID, Query } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { appwriteConfig } from "../appwrite/config";
+import { parseStringify } from "../utils";
+import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
+import { redirect } from "next/navigation";
+
+
+
+const getUserByEmail = async (email:string) => {
+    const { databases } = await createAdminClient();
+
+    const  results = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.usersCollectionId,
+        [Query.equal("email",[email])]
+    )
+
+    return results.total > 0 ? results.documents[0] : null;
+}
+
+const handleError = (error: unknown, message: string) => {
+    console.log(error, message);
+    
+    throw error;
+}
+
+export const sendEmailOtp = async ({email}:{email:string;}) => {
+    const { account } = await createAdminClient();
+    try {
+        const session = await account.createEmailToken(ID.unique(), email);
+
+        return session.userId;
+    } catch (error) {
+        handleError(error, "Failed to send email otp")
+    }
+}
+
+export const createAccount = async ({fullName,email}: {fullName:string; email:string;}) => {
+    const existingUser = await getUserByEmail(email);
+    const accountId = await sendEmailOtp({email});
+    if(!accountId) throw new Error("Failed to send an OTP")
+    
+    if(!existingUser){
+        const { databases } = await createAdminClient();
+
+        await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.usersCollectionId,
+            ID.unique(),
+            {
+                fullName,
+                email,
+                avatar: "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?semt=ais_hybrid",
+                accountId,
+            }
+        )
+    }
+    return parseStringify({ accountId });
+}   
+
+export const verifiySecret = async ({accountId,password}:{accountId:string; password:string;}) => {
+    try {
+        const {account} = await createAdminClient();
+
+        const session = await account.createSession(accountId, password);
+
+        (await cookies()).set('appwrite-session', session.secret, {
+            path:'/', 
+            httpOnly:true,
+            sameSite:'strict',
+            secure:true
+        } );
+
+        return parseStringify({sessionId: session.$id})
+    } catch (error) {
+        handleError(error,"Faild to verify OTP")
+    }
+}
+
+export const getCurrentUser = async () => {
+    const {databases,account} = await createSessionClient();
+
+    const result = await account.get()
+
+    const user = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.usersCollectionId,
+        [Query.equal("accountId",[result.$id])],
+    )
+
+    if(user.total <= 0) return null;
+
+    return parseStringify(user.documents[0]);
+}
+
+export const signOutUser = async () => {
+    const { account } = await createSessionClient();
+    try {
+        await account.deleteSession('current');
+
+        (await cookies()).delete("appwrite-session");
+
+      } catch (error) {
+        handleError(error,"failed to sign out user");
+      } finally {
+        redirect('/sign-in');
+      }
+}
+
+export const signInUser = async ({email}:{email:string}) => {
+    try {
+        const existingUser = await getUserByEmail(email);
+
+        //User exists send OTP
+        if(existingUser){
+           await sendEmailOtp({email});
+           return parseStringify({accountId: existingUser.accountId})
+        }
+
+        return parseStringify({accountId: null, error: "User not found"})
+    } catch (error) {
+        handleError(error,"Failed to sign in user")
+    }
+}
